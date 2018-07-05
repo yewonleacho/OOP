e8690951da5a098c9a3a1fc6b49e79d1d1717fb7
Yewon <yc2654@nyu.edu>
2018-07-05 16:19:45 +0000

    First commit

---
+++ input2.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Please enter your name:\n";
+	string first_name;
+	int age;
+	cin >> first_name;
+	cin >> age;
+	cout << first_name << age;
+}
